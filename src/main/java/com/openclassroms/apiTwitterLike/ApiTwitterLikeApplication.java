3d2ea175e68462a24081820a1322e40013e14a15
EspoirNorbert <espoirnorbert@gmail.com>
2021-08-31 12:52:27 +0200

    demarage du projet

---
+++ src/main/java/com/openclassroms/apiTwitterLike/ApiTwitterLikeApplication.java
@@ -0,0 +1,13 @@
+package com.openclassroms.apiTwitterLike;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiTwitterLikeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiTwitterLikeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/openclassroms/apiTwitterLike/ApiTwitterLikeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.openclassroms.apiTwitterLike;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiTwitterLikeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
